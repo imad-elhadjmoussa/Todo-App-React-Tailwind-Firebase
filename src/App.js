146ca74b9f41dd98514545836bc5cde4a264a599
unknown <imadelhadjmoussa20@gmail.com>
2024-03-02 23:11:42 +0100

    adding some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useRef, useState } from 'react'
 import './index.css';
-import { Button } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import { Todo } from './Components/Todo';
+import { Flag } from '@mui/icons-material';
 
 function App() {
 
   const inputRef = useRef(null)
+
 
   //State to hold todos
   const [todos, setTodos] = useState([]);
@@ -22,17 +24,20 @@ function App() {
   const addTodos = (e) => {
     if (input !== null) {
 
-      let todo={
-        id: todos.length === 0 ? 0 :todos[todos.length-1].id + 1,
-        todo:input, 
+      let todo = {
+        id: todos.length === 0 ? 0 : todos[todos.length - 1].id + 1,
+        todo: input,
       }
 
-      setTodos((prev)=>{
-        return [...prev,todo]
+      setTodos((prev) => {
+        return [...prev, todo]
+      })
+      console.log(input)
+      setInput(() => {
+        return null;
       })
 
-      setInput(null)
-      inputRef.current.value ='';
+      inputRef.current.value = '';
 
     } else {
 
@@ -42,17 +47,44 @@ function App() {
 
   }
 
-  return (
-    <div className=''>
-      <input type="text" placeholder='Add Todo' ref={inputRef} onChange={(e) => { getInput(e) }} />
+  const deleteTodo=(e)=>{
+    let newTodos=todos.filter((ele)=>{
+      return ele.id !== +e.target.id;
+    })
+    setTodos(newTodos);
+  }
 
-      <Button variant="contained" onClick={e => addTodos(e)}>
-        Add todo
-      </Button>
-      <div>
+  return (
+    <div className=' app flex flex-col  '>
+
+      <div className='flex gap-20 md:gap-96 sm:gap-40 justify-between items-center py-10'>
+        {/* <input
+          className='px-3 outline-none py-1 rounded-lg '
+          type="text"
+          placeholder='Add Todo'
+          ref={inputRef}
+          onChange={(e) => { getInput(e) }}
+        /> */}
+        <TextField
+          className='text-white'
+
+          startColor="secondary"
+          id="standard-basic"
+          ref={inputRef}
+          label="Add Todo"
+          focused
+          variant="standard"
+          onChange={(e) => { getInput(e) }}
+        />
+        <Button variant="contained" onClick={e => addTodos(e)}>
+          Add todo
+        </Button>
+      </div>
+
+      <div className='px-5 flex flex-col gap-5'>
         {
           todos.map((ele) => {
-            return (<Todo key={ele.id} todo={ele.todo} />);
+            return (<Todo key={ele.id} todo={ele.todo} id={ele.id} deleteTodo={deleteTodo} />);
           })
         }
       </div>
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Button, DeleteIcon, label, Checkbox } from '@mui/material'
+//import {DeleteIcon} from '@mui/icons-material';
 
-export const Todo = ({ todo }) => {
+
+export const Todo = ({ id, todo , deleteTodo }) => {
     return (
 
-        <div className='text-white flex justify-between w-48 bg-secndry'>
-            <p>
+        <div
+            className='text-white cursor-pointer hover:shadow-2xl transition duration-200   text-lg rounded-lg shadow-lg  items-center p-3 flex justify-between  bg-secndry'
+
+        >
+            <p className='w-10'>
                 {todo}
             </p>
-            <div>
-                Delete
+            <div className='flex flex-col gap-3 sm:flex-row sm:gap-3 '>
+                <Button variant="contained" color="success">
+                    Completed
+                </Button>
+                <Button
+                    id={id}
+                    variant="outlined"
+                    color="error"
+                    onClick={(e)=>{deleteTodo(e)}}
+                >
+                    Delete
+                </Button>
             </div>
         </div>
     )
